8abc6461d474fa73c89ed610eea0463967f35cc6
HanseongBae <bha4388@naver.com>
2024-11-11 17:25:04 +0900

    라이브러리 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StringTest {
     @Test
