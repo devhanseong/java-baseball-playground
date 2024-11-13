5d77a466441c1d961d2cc471b05952216d190a3f
HanseongBae <bha4388@naver.com>
2024-11-13 19:20:26 +0900

    연산자 생성 로직 및 테스트

--- src/main/java/calculator/OperatorType.java
+++ src/main/java/calculator/OperatorType.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Arrays;
+
 public enum OperatorType {
     ADD("더하기", "+", (a, b) -> a + b),
     SUBTRACT("빼기", "-", (a, b) -> a - b),
@@ -18,6 +20,13 @@ public enum OperatorType {
 
     public int operate(final int a, final int b) {
         return calculable.apply(a, b);
+    }
+
+    public static OperatorType from(String userOperation) {
+        return Arrays.stream(values())
+                .filter(each -> each.operation.equals(userOperation))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 연산자입니다."));
     }
 
 }
--- src/test/java/calculator/OperatorTypeTest.java
+++ src/test/java/calculator/OperatorTypeTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,10 +9,11 @@ import java.util.stream.Stream;
 
 import static calculator.OperatorType.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OperatorTypeTest {
 
-    private static Stream<Arguments> setUp() {
+    private static Stream<Arguments> operateArguments() {
         return Stream.of(
                 Arguments.of(2, ADD, 3, 5),
                 Arguments.of(10, SUBTRACT, 6, 4),
@@ -20,8 +22,17 @@ public class OperatorTypeTest {
         );
     }
 
+    private static Stream<Arguments> fromArguments() {
+        return Stream.of(
+                Arguments.of("+", ADD),
+                Arguments.of("-", SUBTRACT),
+                Arguments.of("*", MULTIPLY),
+                Arguments.of("/", DIVIDE)
+        );
+    }
+
     @ParameterizedTest
-    @MethodSource("setUp")
+    @MethodSource("operateArguments")
     void operate(int firsNumber, OperatorType operatorType, int secondNumber, int expect) {
         //given
         //when
@@ -31,4 +42,27 @@ public class OperatorTypeTest {
         assertThat(result).isEqualTo(expect);
     }
 
+    @ParameterizedTest
+    @MethodSource("fromArguments")
+    void from(String operation, OperatorType type) {
+        //given
+        //when
+        OperatorType result = OperatorType.from(operation);
+
+        //then
+        assertThat(result).isEqualTo(type);
+    }
+
+    @Test
+    void unsupportedOperation() {
+        //given
+        String operation = "%";
+
+        //when
+        //then
+        assertThatThrownBy(() -> OperatorType.from(operation))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지원하지 않는 연산자입니다.");
+
+    }
 }
